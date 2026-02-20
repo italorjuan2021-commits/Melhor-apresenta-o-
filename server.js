897d4c6f2e0e36545eaba53d585c4f6a7ae69ca4
Melhor apresentação <italorjuan2021@gmail.com>
2026-02-19 22:23:55 -0300

    Melhor apresentação

--- server.js
+++ server.js
@@ -9,11 +9,70 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 const PORT = process.env.PORT || 3000;
+
+const rooms = {};
+
+function generateRoomCode() {
+  return Math.random().toString(36).substring(2, 8).toUpperCase();
+}
+
+io.on("connection", (socket) => {
+  console.log("Novo jogador conectado:", socket.id);
+
+  socket.on("createRoom", ({ nickname }) => {
+    const roomCode = generateRoomCode();
+
+    rooms[roomCode] = {
+      players: [],
+      host: socket.id,
+      gameStarted: false
+    };
+
+    rooms[roomCode].players.push({
+      id: socket.id,
+      nickname,
+      score: 0
+    });
+
+    socket.join(roomCode);
+
+    socket.emit("roomCreated", { roomCode });
+    io.to(roomCode).emit("updatePlayers", rooms[roomCode].players);
+  });
+
+  socket.on("joinRoom", ({ nickname, roomCode }) => {
+    if (!rooms[roomCode]) return;
+
+    rooms[roomCode].players.push({
+      id: socket.id,
+      nickname,
+      score: 0
+    });
+
+    socket.join(roomCode);
+
+    io.to(roomCode).emit("updatePlayers", rooms[roomCode].players);
+  });
+
+  socket.on("startGame", ({ roomCode }) => {
+    if (!rooms[roomCode]) return;
+    io.to(roomCode).emit("gameStarted");
+  });
+
+  socket.on("disconnect", () => {
+    for (const roomCode in rooms) {
+      const room = rooms[roomCode];
+      room.players = room.players.filter(p => p.id !== socket.id);
+
+      io.to(roomCode).emit("updatePlayers", room.players);
+
+      if (room.players.length === 0) {
+        delete rooms[roomCode];
+      }
+    }
+  });
+});
 
 server.listen(PORT, () => {
   console.log("Servidor rodando na porta", PORT);
